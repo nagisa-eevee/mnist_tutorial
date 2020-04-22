409005a9e4cd8a01f116aab3e42668db48a757d5
nagisa-eevee <46982956+nagisa-eevee@users.noreply.github.com>
2020-04-22 15:49:36 +0800

    Update pytorch.py

--- pytorch.py
+++ pytorch.py
@@ -35,19 +35,17 @@ class SimpleNet(nn.Module):
         # input size 28*28
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        # self.dropout1 = nn.Dropout2d(p=0.25)
-        # self.dropout2 = nn.Dropout2d(p=0.5)
-        self.fc1 = nn.Linear(in_features=64*12*12, out_features=128)
+        self.dropout1 = nn.Dropout2d(p=0.5)
+        self.fc1 = nn.Linear(in_features=64*11*11, out_features=128)
         self.fc2 = nn.Linear(in_features=128, out_features=10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        # x = self.dropout1(x)
+        x = F.relu(self.conv2(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        # x = self.dropout2(x)
+        x = self.dropout1(x)
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
         return x
